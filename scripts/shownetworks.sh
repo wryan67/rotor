7d84db8be3b21c217f9ed006b7d5badceb474a5c
Wade Ryan <wryan67@gmail.com>
2021-06-19 18:10:11 -0500

    wifi frequency update

--- scripts/shownetworks.sh
+++ scripts/shownetworks.sh
@@ -3,8 +3,10 @@
 sudo iwlist wlan0 scan 2>/dev/null | \
 gawk '{
     if ($1=="Cell" && ssid!="") {
-       print str "," ssid
+       print str "þ" ssid "þ" freq, hz
        ssid=""
+       freq=""
+       hz=""
     } 
     
     if (match($0, /ESSID:"(.*)"/, a)>0) {
@@ -12,12 +14,16 @@ gawk '{
         ssid=a[1]
       }
     }
+    if (match($0, /Frequency: *(.*) ([a-zA-Z]Hz)/, a)>0) {
+      freq=sprintf("%.1f", a[1]);
+      hz=a[2]
+    }
     if (match($0, /Signal level=(.*) dBm/, a)>0) {
       str=a[1]
     }
     }END{
        if (ssid!="") {
-         print str "," ssid
+         print str "þ" ssid "þ" freq, hz
        }
     }' | sort -t, -rn 2> /dev/null
 
